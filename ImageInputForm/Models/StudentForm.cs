27d603623af38a3162cae85ef73db3e8688e9a2d
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-07-28 11:58:14 +0530

    model for upload image

---
+++ ImageInputForm/Models/StudentForm.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ImageInputForm.Models
+{
+    public class StudentForm
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public IFormFile? Image { get; set; } // Attribute for uploading image
+    }
+}
